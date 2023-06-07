0c6342ea6fd4aaddf0617ca4212549ff10059acc
monkeycs60 <clement.serizay@gmail.com>
2023-06-07 15:33:41 +0200

    chore: updating settings and configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				// titles: ['"Raleway"', 'sans-serif'],
+				// body: ['"DM Serif Display"', 'serif'],
+				// script: ['"Sugeng-Rawuh"', 'sans-serif'],
+			},
+			colors: {
+				madeonaBrown: {
+					100: '#f5eee6',
+					200: '#e0ccc0',
+					300: '#cab5a2',
+					400: '#9a7365',
+					500: '#b9907a',
+					600: '#5d4239',
+					700: '#9b684d',
+				},
+			},
+			screens: {
+				'3xl': '1700px',
+			},
+		},
+		plugins: [],
+	},
+};
