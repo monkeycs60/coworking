3a7982e09f82368e12142ce4b124dd7d323800fd
monkeycs60 <clement.serizay@gmail.com>
2023-06-30 19:20:41 +0200

    fix: update new response

--- src/app/api/coworking/route.tsx
+++ src/app/api/coworking/route.tsx
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest, res: NextResponse) {
-    const placeId = new URL(req.url).searchParams.get('placeId');
+	const placeId = new URL(req.url).searchParams.get('placeId');
 	const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 
 	const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=geometry&key=${googleMapsApiKey}`;
@@ -9,7 +9,11 @@ export async function GET(req: NextRequest, res: NextResponse) {
 	const response = await fetch(url);
 	const data = await response.json();
 	console.log('data du details', data);
-	
 
-	return new Response('ok ça marche le fetch détaillé');
+	return new Response('ok ça marche le fetch détaillé', {
+		status: 200,
+		headers: {
+			'content-type': 'application/json',
+		},
+	});
 }
