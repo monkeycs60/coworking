d7ceb4bc62baf6be8207f31ec56ae32e8e767cc0
monkeycs60 <clement.serizay@gmail.com>
2023-06-30 18:15:34 +0200

    debug: add a log to inspect api places

--- src/components/maps/PlacesAutocomplete.tsx
+++ src/components/maps/PlacesAutocomplete.tsx
@@ -3,9 +3,13 @@
 import { useRef, useState } from 'react';
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
+type Option = {
+	value: string;
+	label: string;
+};
 
 const PlacesAutocomplete = () => {
- 
+  const [value, setValue] = useState<Option | null>(null);
 
 	const googleMapsApiKey: string | undefined =
 		process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
@@ -14,13 +18,17 @@ const PlacesAutocomplete = () => {
 		return <div>Missing Google Maps API Key</div>;
 	}
 
-
+	console.log(value);
 	
 
 	return (
 		<div>
 			<GooglePlacesAutocomplete
 				apiKey={googleMapsApiKey}
+				selectProps={{
+					value,
+					onChange: (newValue: Option | null) => setValue(newValue),
+				}}
 			/>
 		</div>
 	);
