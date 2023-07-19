11b5548b5ad5e26cfa211ca037304db95dad6434
monkeycs60 <clement.serizay@gmail.com>
2023-07-19 14:00:01 +0200

    style: responsive mobile for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,39 +6,39 @@ import clsx from 'clsx';
 export default function Home() {
 	return (
 		<section className='overall-bg flex h-screen w-full flex-col justify-center'>
-			<div className='mt-[27vh] flex h-full gap-4 '>
-				<div className='flex w-[55%] flex-col '>
+			<div className='mt-[16vh] flex h-full flex-col gap-4 lg:mt-[27vh] lg:flex-row '>
+				<div className='flex flex-col px-4 lg:w-[55%] lg:px-0'>
 					<div className='flex flex-col gap-4'>
-						<h2 className=' text-4xl font-bold leading-normal 3xl:text-5xl 3xl:leading-relaxed'>
+						<h2 className='text-3xl font-bold leading-normal lg:text-4xl 3xl:text-5xl 3xl:leading-relaxed'>
 							Découvrez des lieux conviviaux où coworker gratuitement !
 						</h2>
-						<h3 className='w-[75%] text-gray-700 3xl:text-lg'>
+						<h3 className='text-sm text-gray-700 lg:w-[75%] lg:text-base 3xl:text-lg'>
 							Grâce à notre communauté, trouvez les bons plans pour
 							travailler malin dans la ville de votre choix.
 						</h3>
 					</div>
-					<div className='flex flex-col gap-3 pt-14'>
+					<div className='flex flex-col gap-3 pt-8 lg:pt-14'>
 						<form action=''>
-							<div className='relative flex items-center gap-2'>
+							<div className='relative flex flex-col items-center gap-2 lg:flex-row'>
 								<MapPin
 									size={20}
-									className='absolute left-4 text-gray-600'
+									className='absolute left-4 top-3 text-gray-600 3xl:top-4'
 								/>
 								<input
 									type='text'
 									placeholder='Entrez une ville'
-									className='rounded-xl border-2 border-gray-500 px-20 py-2 indent-0 3xl:px-28 3xl:py-4'
+									className='w-full rounded-xl border-2 border-gray-500 px-4 py-2 indent-8 lg:w-auto lg:px-20 lg:py-2 lg:indent-0 3xl:px-28 3xl:py-3'
 								/>
 								<Button
 									variant={'default'}
 									size={'sm'}
-									className='3xl:px-6 3xl:py-4'>
+									className='w-full lg:w-auto 3xl:px-6 3xl:py-3'>
 									<Image
 										src={'/search-refraction.svg'}
 										alt='search'
 										width={20}
 										height={20}
-										className='pointer-events-none 3xl:h-[26px] 3xl:w-[26px] '
+										className='pointer-events-none lg:h-[20px] lg:w-[20px] 3xl:h-[26px] 3xl:w-[26px]'
 									/>
 								</Button>
 							</div>
@@ -49,10 +49,11 @@ export default function Home() {
 						</p>
 					</div>
 				</div>
-				<div className='flex w-[45%] justify-end '>
+				<div className='mt-[5vh] flex justify-center lg:mt-0 lg:w-[45%] lg:justify-end '>
 					<div
 						className={clsx(
-							'relative flex h-[80%] w-[90%] justify-end',
+							'relative flex h-[200px] w-[300px]',
+							'lg:h-[80%] lg:w-[90%] lg:justify-end',
 							'3xl:h-[55%] 3xl:w-[100%] '
 						)}>
 						<Image
@@ -66,14 +67,21 @@ export default function Home() {
 							height={360}
 							src='/Hand-drawn-arrow.svg'
 							alt='Coworkers autour d un laptop'
-							className='absolute -bottom-20 -left-64 z-20 3xl:-bottom-32 3xl:pointer-events-none'
+							className='absolute -bottom-20 -left-64 z-20 hidden lg:block 3xl:pointer-events-none 3xl:-bottom-32'
 						/>
 						<Image
 							width={160}
 							height={160}
 							src='/Ellipse-yellow.svg'
 							alt='Coworkers autour d un laptop'
-							className='absolute -bottom-16 right-0'
+							className='absolute -right-4 -top-8 lg:-bottom-16 lg:right-0'
+						/>
+						<Image
+							width={160}
+							height={160}
+							src='/Ellipse-blue.svg'
+							alt='Coworkers autour d un laptop'
+							className='absolute -bottom-8 -left-4 lg:hidden'
 						/>
 					</div>
 				</div>
--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -4,19 +4,20 @@ import { Button } from '../ui/button';
 import Image from 'next/image';
 import Link from 'next/link';
 import SignUpButtonLogic from '../auth/SignUpButtonLogic';
+import { Menu } from 'lucide-react';
 
 async function NavBar() {
 	const { userId } = await auth();
 	return (
-		<div className='fixed z-10 mt-4 flex w-[1200px] justify-between gap-10  py-2'>
-			<div className='flex items-center justify-center gap-14'>
+		<div className='fixed z-10 mt-4 flex w-full justify-between  gap-10 py-2 lg:w-[1200px]'>
+			<div className='flex w-full items-center justify-between px-4 lg:w-auto lg:justify-center lg:gap-14 lg:px-0'>
 				<Link href={'/'} className='flex items-center justify-center gap-4'>
-						<Image
-							src={'/logo-cowork-v1-nobg.png'}
-							alt='logo'
-							width={40}
-							height={40}
-						/>
+					<Image
+						src={'/logo-cowork-v1-nobg.png'}
+						alt='logo'
+						width={40}
+						height={40}
+					/>
 					<h1 className='font-telma text-xl font-semibold first-letter:text-primary'>
 						Coworkez{' '}
 						<span className='block first-letter:text-secondary'>
@@ -24,7 +25,7 @@ async function NavBar() {
 						</span>{' '}
 					</h1>
 				</Link>
-				<ul className='flex items-center justify-center gap-6 font-semibold'>
+				<ul className='hidden items-center justify-center gap-6 font-semibold lg:flex'>
 					<li className='linkHoverEffect'>
 						<Link href={'/about'}>A propos</Link>
 					</li>
@@ -38,8 +39,11 @@ async function NavBar() {
 						<Link href={'/add-coworking'}>Ajouter un spot</Link>
 					</li>
 				</ul>
+				<div className='block lg:hidden'>
+					<Menu className='text-primary' />
+				</div>
 			</div>
-			<div className='flex items-center justify-center gap-4'>
+			<div className='hidden items-center justify-center gap-4 lg:flex'>
 				{userId ? (
 					<>
 						<SignOutButton>
