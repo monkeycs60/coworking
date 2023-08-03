0be76bdd8ac91b6db5602341ff7aec6f0d29ab99
Clement SERIZAY <clement.serizay@gmail.com>
2023-08-03 13:53:02 +0200

    chore: adjust middleware to add a public route
    contact route is now public

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-	publicRoutes: ['/', '/coworking-list'],
+	publicRoutes: ['/', '/coworking-list', 'api/contact'],
 });
 
 export const config = {
