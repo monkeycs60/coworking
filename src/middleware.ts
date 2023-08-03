fd3912f41769286bc95d2193aea028a01dea3aa8
Clement SERIZAY <clement.serizay@gmail.com>
2023-08-03 14:04:25 +0200

    fix: add a / wrong spelling of the route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-	publicRoutes: ['/', '/coworking-list', 'api/contact'],
+	publicRoutes: ['/', '/coworking-list', '/api/contact'],
 });
 
 export const config = {
