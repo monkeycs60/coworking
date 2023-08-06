9b5c43e4693c11e493ccbd0ad535118094b430a7
monkeycs60 <clement.serizay@gmail.com>
2023-08-06 17:36:24 +0200

    style(responsive): assets tablet & desk

--- src/components/home/sections/Assets.tsx
+++ src/components/home/sections/Assets.tsx
@@ -17,7 +17,7 @@ const Assets = () => {
 	}, [asset.imgSource]);
 
 	return (
-		<section className='my-[5vh] flex flex-col gap-8 overflow-x-hidden lg:h-[100vh]'>
+		<section className='my-[5vh] flex flex-col gap-8 overflow-hidden'>
 			<div className='flex flex-col gap-2 px-4'>
 				<h2 className='text-2xl font-bold'>Les atouts Coworkez Malin</h2>
 				<h3 className='text-base text-gray-700 lg:w-[75%] lg:text-base 3xl:text-lg'>
@@ -25,105 +25,109 @@ const Assets = () => {
 					votre ville et de nouvelles façons de travailler
 				</h3>
 			</div>
-			<div className='flex gap-4 px-4'>
-				<div className='flex w-1 flex-col'>
-					<div
-						className={`grow transition-all duration-300 ${
-							asset.selected === 'flexibility'
-								? 'bg-primary'
-								: 'bg-gray-300'
-						}`}
-						onMouseEnter={() => {
-							setAsset({
-								selected: 'flexibility',
-								imgSource: '/man-phonecall.jpg',
-								imgAlt: 'homme au téléphone',
-							});
-						}}
-					/>
-					<div
-						className={`grow transition-all duration-300 ${
-							asset.selected === 'share' ? 'bg-primary' : 'bg-gray-300'
-						}`}
-						onMouseEnter={() => {
-							setAsset({
-								selected: 'share',
-								imgSource: '/share.jpg',
-								imgAlt: 'deux personnes en entretien',
-							});
-						}}
-					/>
-					<div
-						className={`grow transition-all duration-300 ${
-							asset.selected === 'free' ? 'bg-primary' : 'bg-gray-300'
-						}`}
-						onMouseEnter={() => {
-							setAsset({
-								selected: 'free',
-								imgSource: '/free.jpg',
-								imgAlt: 'porte-monnaie vide',
-							});
-						}}
+			<div className='flex flex-col gap-8 lg:flex-row xl:gap-20 items-center'>
+				<div className='flex gap-4 px-4 lg:h-[370px]'>
+					<div className='flex w-1 flex-col'>
+						<div
+							className={`grow transition-all duration-300 ${
+								asset.selected === 'flexibility'
+									? 'bg-primary'
+									: 'bg-gray-300'
+							}`}
+							onMouseEnter={() => {
+								setAsset({
+									selected: 'flexibility',
+									imgSource: '/man-phonecall.jpg',
+									imgAlt: 'homme au téléphone',
+								});
+							}}
+						/>
+						<div
+							className={`grow transition-all duration-300 ${
+								asset.selected === 'share'
+									? 'bg-primary'
+									: 'bg-gray-300'
+							}`}
+							onMouseEnter={() => {
+								setAsset({
+									selected: 'share',
+									imgSource: '/share.jpg',
+									imgAlt: 'deux personnes en entretien',
+								});
+							}}
+						/>
+						<div
+							className={`grow transition-all duration-300 ${
+								asset.selected === 'free' ? 'bg-primary' : 'bg-gray-300'
+							}`}
+							onMouseEnter={() => {
+								setAsset({
+									selected: 'free',
+									imgSource: '/free.jpg',
+									imgAlt: 'porte-monnaie vide',
+								});
+							}}
+						/>
+					</div>
+					<div className='flex flex-1 flex-col gap-4 px-4 py-2 lg:h-[370px] lg:justify-between'>
+						<div
+							className='group relative hover:border-primary'
+							onMouseEnter={() => {
+								setAsset({
+									selected: 'flexibility',
+									imgSource: '/man-phonecall.jpg',
+									imgAlt: 'homme au téléphone',
+								});
+							}}>
+							<h4 className='font-bold'>Flexibilité</h4>
+							<p>
+								Venez profiter sans prise de tête de nos
+								d’établissements repertoriés. Travaillez où vous voulez,
+								quand vous voulez et avec qui vous voulez.
+							</p>
+						</div>
+						<div
+							className='group relative hover:border-primary'
+							onMouseEnter={() => {
+								setAsset({
+									selected: 'share',
+									imgSource: '/share.jpg',
+									imgAlt: 'deux personnes en entretien',
+								});
+							}}>
+							<h4 className='font-bold'>Partage</h4>
+							<p>
+								Partagez vos meilleures adresses et commentez celles des
+								autres. Vous n’êtes qu’à un clic de rencontrer de
+								nouveaux coworkers.
+							</p>
+						</div>
+						<div
+							className='group relative hover:border-primary'
+							onMouseEnter={() => {
+								setAsset({
+									selected: 'free',
+									imgSource: '/free.jpg',
+									imgAlt: 'porte-monnaie vide',
+								});
+							}}>
+							<h4 className='font-bold'>Gratuité</h4>
+							<p>
+								Pas de frais d’abonnement, pas de coûts de mise en
+								relation. Votre seul investissement, c’est votre café
+								latte.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className='relative h-[250px] w-screen sm:h-[380px] lg:h-[370px] 2xl:h-[430px] '>
+					<Image
+						src={asset.imgSource}
+						alt={asset.imgAlt}
+						fill={true}
+						priority={true}
 					/>
 				</div>
-				<div className='flex flex-1 flex-col gap-4 px-4 py-2'>
-					<div
-						className='group relative hover:border-primary'
-						onMouseEnter={() => {
-							setAsset({
-								selected: 'flexibility',
-								imgSource: '/man-phonecall.jpg',
-								imgAlt: 'homme au téléphone',
-							});
-						}}>
-						<h4 className='font-bold'>Flexibilité</h4>
-						<p>
-							Venez profiter sans prise de tête de nos d’établissements
-							repertoriés. Travaillez où vous voulez, quand vous voulez
-							et avec qui vous voulez.
-						</p>
-					</div>
-					<div
-						className='group relative hover:border-primary'
-						onMouseEnter={() => {
-							setAsset({
-								selected: 'share',
-								imgSource: '/share.jpg',
-								imgAlt: 'deux personnes en entretien',
-							});
-						}}>
-						<h4 className='font-bold'>Partage</h4>
-						<p>
-							Partagez vos meilleures adresses et commentez celles des
-							autres. Vous n’êtes qu’à un clic de rencontrer de nouveaux
-							coworkers.
-						</p>
-					</div>
-					<div
-						className='group relative hover:border-primary'
-						onMouseEnter={() => {
-							setAsset({
-								selected: 'free',
-								imgSource: '/free.jpg',
-								imgAlt: 'porte-monnaie vide',
-							});
-						}}>
-						<h4 className='font-bold'>Gratuité</h4>
-						<p>
-							Pas de frais d’abonnement, pas de coûts de mise en
-							relation. Votre seul investissement, c’est votre café
-							latte.
-						</p>
-					</div>
-				</div>
-			</div>
-			<div className='relative h-[250px] w-screen'>
-				<Image
-					src={asset.imgSource}
-					alt={asset.imgAlt}
-					fill={true}
-					priority={true}
-				/>
 			</div>
 		</section>
 	);
