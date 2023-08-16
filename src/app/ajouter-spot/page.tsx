989f1cb65aa95da959f53ea0af23429eeb4747f5
monkeycs60 <clement.serizay@gmail.com>
2023-08-16 11:54:32 +0200

    style: change add spot style

--- src/app/ajouter-spot/page.tsx
+++ src/app/ajouter-spot/page.tsx
@@ -1,10 +1,6 @@
-import React from 'react';
-import GoogleMapElement from '@/components/maps/GoogleMapElement';
-import PlacesAutocomplete from '@/components/maps/PlacesAutocomplete';
-
 const page = () => {
 	return (
-		<div className='bg-zinc-500'>
+		<section className='mt-16 bg-zinc-500 lg:mt-32 xl:mt-48 3xl:mt-64'>
 			<div className='mt-4 flex flex-col justify-center text-center'>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -13,24 +9,7 @@ const page = () => {
 				</p>
 				<p>Liste de tous les coworkings</p>
 			</div>
-			<div className='flex justify-between p-8'>
-				<GoogleMapElement
-					height='400px'
-					width='500px'
-					centerMap={{ lat: 46.603354, lng: 1.888334 }}
-					zoom={5}
-				/>
-				<div className='flex w-1/2 flex-col gap-4 bg-red-400'>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-						Enim, dicta! Hic sed porro ad sequi quae quibusdam dolores
-						qui, iure deserunt fugit suscipit rerum ducimus, labore sunt
-						magni atque. Asperiores.
-					</p>
-					<PlacesAutocomplete />
-				</div>
-			</div>
-		</div>
+		</section>
 	);
 };
 
