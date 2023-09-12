0c55c364168c661b84f0d203210265ec984acd0d
Clement SERIZAY <clement.serizay@gmail.com>
2023-09-12 14:40:08 +0200

    refactor; create a coworking card

--- src/app/explore/[citySelected]/page.tsx
+++ src/app/explore/[citySelected]/page.tsx
@@ -3,6 +3,7 @@ import { getAllCoworkingsByCity } from '@/services/getAllCoworkingsByCity';
 import Map from '@/components/explore/Map';
 import { getCityCenter } from '@/services/getCityCenter';
 import getCoworkingsCoords from '@/lib/functions/getCoworkingsCoords';
+import CoworkingCard from '@/components/explore/cards/CoworkingCard';
 
 const page = async ({
     params: { citySelected },
@@ -29,21 +30,7 @@ const page = async ({
             />
             <div className='flex max-h-[700px] w-full flex-wrap gap-8 overflow-y-auto bg-slate-500 p-4'>
                 {coworkings.map((coworking) => (
-                    <div
-                        key={coworking.id}
-                        className='flex h-[300px] w-[30%] flex-col items-center gap-4 bg-cover bg-center text-center'
-                        style={{
-                            backgroundImage: `url(${coworking.imagesSelected[0].url})`,
-                        }}
-                    >
-                        <div className='z-50 h-[160px] w-full space-y-3 bg-slate-400/80 text-white'>
-                            <p className='font-semibold'>{coworking.name}</p>
-                            <p className='text-sm'>{coworking.address}</p>
-                            <p className='line-clamp-4 text-sm'>
-                                {coworking.description}
-                            </p>
-                        </div>
-                    </div>
+                    <CoworkingCard coworking={coworking} key={coworking.id} />
                 ))}
             </div>
         </section>
