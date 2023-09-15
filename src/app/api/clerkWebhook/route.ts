26197a06f60746e3ebef410e3d380c1664c64686
monkeycs60 <clement.serizay@gmail.com>
2023-09-15 12:07:43 +0200

    chore: test smt

--- src/app/api/clerkWebhook/route.ts
+++ src/app/api/clerkWebhook/route.ts
@@ -4,7 +4,7 @@ import { WebhookEvent } from '@clerk/nextjs/server';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
-
+//test 
 export async function POST(req: Request) {
     // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
     const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
