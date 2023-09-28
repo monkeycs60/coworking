e591e68b43f468933bdc2cd2e91cc9361057c793
Clement SERIZAY <clement.serizay@gmail.com>
2023-09-28 16:01:30 +0200

    fix: close hamburger when sign mobile

--- src/components/layout/Hamburger.tsx
+++ src/components/layout/Hamburger.tsx
@@ -3,100 +3,111 @@
 import { motion } from 'framer-motion';
 import { X } from 'lucide-react';
 import {
-	SignOutButton,
-	useAuth,
-	SignInButton,
-	SignUpButton,
-	UserButton,
+    SignOutButton,
+    useAuth,
+    SignInButton,
+    SignUpButton,
+    UserButton,
 } from '@clerk/nextjs';
 import Link from 'next/link';
 
 interface HamburgerProps {
-	isHamburgerOpen: boolean;
-	toggleHamburgerMenu: () => void;
+    isHamburgerOpen: boolean;
+    toggleHamburgerMenu: () => void;
 }
 const Hamburger = ({
-	isHamburgerOpen,
-	toggleHamburgerMenu,
+    isHamburgerOpen,
+    toggleHamburgerMenu,
 }: HamburgerProps) => {
-	const { userId } = useAuth();
+    const { userId } = useAuth();
 
-	const variants = {
-		open: { opacity: 1, y: 0 },
-		closed: { opacity: 0, y: '100%' },
-		exit: { opacity: 0, y: '100%' },
-	};
-	return (
-		<motion.section
-			initial='closed'
-			animate={isHamburgerOpen ? 'open' : 'closed'}
-			exit='exit'
-			variants={variants}
-			transition={{ duration: 0.5 }}
-			className='fixed left-0 top-0 z-[100] h-screen w-screen overflow-hidden bg-white font-inter'>
-			<div className=' flex h-full flex-col  justify-center p-12 text-center text-lg font-semibold sm:text-xl '>
-				<div className='flex flex-col gap-6'>
-					<Link
-						href={'/about'}
-						className='linkHoverEffect'
-						onClick={toggleHamburgerMenu}>
-						A propos
-					</Link>
-					<Link
-						href={'/'}
-						className='linkHoverEffect'
-						onClick={toggleHamburgerMenu}>
-						FAQ
-					</Link>
-					<Link
-						href={'/ajouter-spot'}
-						className='linkHoverEffect'
-						onClick={toggleHamburgerMenu}>
-						Explorer
-					</Link>
-					<Link
-						href={'/ajouter-spot'}
-						className='linkHoverEffect'
-						onClick={toggleHamburgerMenu}>
-						Ajouter un spot
-					</Link>
-				</div>
+    const variants = {
+        open: { opacity: 1, y: 0 },
+        closed: { opacity: 0, y: '100%' },
+        exit: { opacity: 0, y: '100%' },
+    };
+    return (
+        <motion.section
+            initial='closed'
+            animate={isHamburgerOpen ? 'open' : 'closed'}
+            exit='exit'
+            variants={variants}
+            transition={{ duration: 0.5 }}
+            className='fixed left-0 top-0 z-[100] h-screen w-screen overflow-hidden bg-white font-inter'
+        >
+            <div className=' flex h-full flex-col  justify-center p-12 text-center text-lg font-semibold sm:text-xl '>
+                <div className='flex flex-col gap-6'>
+                    <Link
+                        href={'/about'}
+                        className='linkHoverEffect'
+                        onClick={toggleHamburgerMenu}
+                    >
+                        A propos
+                    </Link>
+                    <Link
+                        href={'/'}
+                        className='linkHoverEffect'
+                        onClick={toggleHamburgerMenu}
+                    >
+                        FAQ
+                    </Link>
+                    <Link
+                        href={'/explore'}
+                        className='linkHoverEffect'
+                        onClick={toggleHamburgerMenu}
+                    >
+                        Explorer
+                    </Link>
+                    <Link
+                        href={'/ajouter-spot'}
+                        className='linkHoverEffect'
+                        onClick={toggleHamburgerMenu}
+                    >
+                        Ajouter un spot
+                    </Link>
+                </div>
 
-				{userId ? (
-					<div className='hamburger-connect-shadow bg mt-24 flex flex-col items-center gap-4 p-6 shadow-xl drop-shadow-xl'>
-						<div className='flex gap-2'>
-							<p>Profil :</p>
-							<UserButton />
-						</div>
-						<SignOutButton>
-							<div className='linkHoverEffect'>
-								<span>Se déconnecter</span>
-							</div>
-						</SignOutButton>
-					</div>
-				) : (
-					<div className='hamburger-connect-shadow bg mt-24 flex flex-col gap-4 p-6 shadow-xl drop-shadow-xl'>
-						<SignInButton>
-							<div className='linkHoverEffect'>
-								<span>Se connecter</span>
-							</div>
-						</SignInButton>
+                {userId ? (
+                    <div className='hamburger-connect-shadow bg mt-24 flex flex-col items-center gap-4 p-6 shadow-xl drop-shadow-xl'>
+                        <div className='flex gap-2'>
+                            <p>Profil :</p>
+                            <UserButton />
+                        </div>
+                        <SignOutButton>
+                            <div className='linkHoverEffect'>
+                                <span>Se déconnecter</span>
+                            </div>
+                        </SignOutButton>
+                    </div>
+                ) : (
+                    <div className='hamburger-connect-shadow bg mt-24 flex flex-col gap-4 p-6 shadow-xl drop-shadow-xl'>
+                        <SignInButton>
+                            <div
+                                className='linkHoverEffect'
+                                onClick={toggleHamburgerMenu}
+                            >
+                                <span>Se connecter</span>
+                            </div>
+                        </SignInButton>
 
-						<SignUpButton>
-							<div className='linkHoverEffect'>
-								<span>S&apos;inscrire</span>
-							</div>
-						</SignUpButton>
-					</div>
-				)}
-			</div>
-			<X
-				size={40}
-				className='absolute right-3 top-4'
-				onClick={toggleHamburgerMenu}
-			/>
-		</motion.section>
-	);
+                        <SignUpButton>
+                            <div
+                                className='linkHoverEffect'
+                                onClick={toggleHamburgerMenu}
+                            >
+                                <span>S&apos;inscrire</span>
+                            </div>
+                        </SignUpButton>
+                    </div>
+                )}
+            </div>
+            <X
+                size={40}
+                className='absolute right-3 top-4'
+                onClick={toggleHamburgerMenu}
+            />
+        </motion.section>
+    );
 };
 
 export default Hamburger;
