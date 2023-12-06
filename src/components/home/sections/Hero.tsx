03d3f0aaadf81f273ab123f8671d48d55708dec7
Clement SERIZAY <clement.serizay@gmail.com>
2023-12-06 17:32:51 +0100

    style: adjust img width and padding

--- src/components/home/sections/Hero.tsx
+++ src/components/home/sections/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = async () => {
     return (
         <section className='overall-bg mb-[10vh] flex w-full flex-col justify-center lg:mb-0 lg:h-screen'>
             <div className='mt-3 flex  flex-col gap-4 sm:mt-20  sm:gap-10 lg:-mt-4 lg:flex-row  lg:gap-0 2xl:-mt-28 2xl:gap-10 3xl:-mt-20 '>
-                <div className='flex flex-col px-4 lg:w-[55%] lg:px-10 2xl:px-10 3xl:w-[50%] 3xl:px-0'>
+                <div className='flex flex-col px-4 lg:w-[55%] 2xl:px-16 3xl:w-[50%] 3xl:px-0'>
                     <div className='flex flex-col gap-4'>
                         <h2 className='text-2xl font-bold leading-normal sm:text-4xl lg:text-4xl 2xl:text-5xl 3xl:leading-relaxed'>
                             Découvrez des lieux conviviaux où coworker
@@ -65,7 +65,7 @@ const Hero = async () => {
                             height={297}
                             src='/Hand-drawn-arrow.svg'
                             alt='fleche jaune'
-                            className='absolute -left-64 bottom-4 z-20 hidden lg:bottom-[-120px] lg:left-[-170px] lg:block lg:w-[260px] 2xl:bottom-[-100px] 2xl:left-[-190px] 2xl:w-[150px] 3xl:pointer-events-none 3xl:bottom-[-90px] 3xl:left-[-170px] 3xl:h-[300px] 3xl:w-[300px] '
+                            className='absolute -left-64 bottom-4 z-20 hidden lg:bottom-[-120px] lg:left-[-170px] lg:block lg:w-[260px] 2xl:bottom-[-80px] 2xl:left-[-150px] 2xl:w-[300px] 3xl:pointer-events-none 3xl:bottom-[-90px] 3xl:left-[-170px] 3xl:h-[300px] 3xl:w-[300px] '
                         />
                         <Image
                             width={160}
