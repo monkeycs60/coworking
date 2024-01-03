961f04f778e68f3f8e60f94f01298174104f7906
monkeycs60 <clement.serizay@gmail.com>
2024-01-03 01:08:31 +0100

    feat: cover photo flag

--- src/components/ajouter-spot/form/inputs/SortablePhoto.tsx
+++ src/components/ajouter-spot/form/inputs/SortablePhoto.tsx
@@ -30,12 +30,36 @@ const SortablePhoto = (props: SortablePhotoProps) => {
         transition,
     };
 
-    return (
+    // if indexof image selected is 0, create a special border around it
+    if (props.imageUrl === imagesSelected[0].url) {
+        return (
+            <div className='relative'>
+                <div ref={setNodeRef} style={style} {...attributes} {...listeners} className='border-4 border-red-500 bg-red-200'>
+                    <img src={props.imageUrl} alt="image" width={200} height={200} />
+                </div >
+                <button
+                    className='absolute right-0 top-0 z-[5000] h-4 w-4 cursor-pointer hover:scale-125'
+                    onClick={(e:
+                        React.MouseEvent<HTMLButtonElement, MouseEvent>
+                    ) => {
+                        e.stopPropagation(); // This stops the click event from propagating to drag-and-drop listeners
+                        console.log('clicked', props.imageUrl);
+
+                        dispatch(removeImageSelectedUrls(props.id));
+                    }
+                    }
+                >
+
+                    <X />
+                </button>
+            </div>
+        )
+    } else return (
         <div className='relative'>
             <div ref={
                 setNodeRef
             } style={style} {...attributes} {...listeners} className='bg-red-200'>
-                    <img src={props.imageUrl} alt="image" width={200} height={200} />
+                <img src={props.imageUrl} alt="image" width={200} height={200} />
             </div >
             <button
                 className='absolute right-0 top-0 z-[5000] h-4 w-4 cursor-pointer hover:scale-125'
