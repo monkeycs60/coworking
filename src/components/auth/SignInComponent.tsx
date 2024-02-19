fa04b59adce64825d9012db048711f79963f312e
monkeycs60 <clement.serizay@gmail.com>
2024-02-19 21:46:50 +0100

    chore: add text

--- src/components/auth/SignInComponent.tsx
+++ src/components/auth/SignInComponent.tsx
@@ -13,6 +13,7 @@ import Link from 'next/link';
 
 const SignInComponent = () => {
     const [waitingToSubmit, setWaitingToSubmit] = useState(false);
+    console.log('waitingToSubmit', waitingToSubmit);
 
     const router = useRouter();
     const { toast } = useToast();
@@ -59,26 +60,50 @@ const SignInComponent = () => {
 
     return (
         <section className='mt-[80px] 3xl:mt-[100px]'>
-            <form onSubmit={handleSubmit(onSubmit)} className='m-auto mt-8 flex w-1/3 flex-col gap-2 rounded-2xl bg-white p-6 text-black'>
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                className='m-auto mt-8 flex w-1/3 flex-col gap-2 rounded-2xl bg-white p-6 text-black'
+            >
                 <h2 className='text-xl font-semibold italic'>Coworker Malin</h2>
                 <div className='m-auto h-[1px] w-full bg-slate-300'></div>
                 <h1 className='font-light'>Connexion</h1>
                 <div className='mt-6 flex flex-col gap-1'>
-                    <label className='text-xs' htmlFor='email'>Email</label>
-                    <input className='rounded-2xl border-[1px] border-slate-300 p-2 ' placeholder='mon@mail.fr' type='email' {...register('email')} />
+                    <label className='text-xs' htmlFor='email'>
+                        Email
+                    </label>
+                    <input
+                        className='rounded-2xl border-[1px] border-slate-300 p-2 '
+                        placeholder='mon@mail.fr'
+                        type='email'
+                        {...register('email')}
+                    />
                     {errors.email && <p>{errors.email.message}</p>}
                 </div>
 
                 <div className='flex flex-col gap-1'>
-                    <label className='text-xs' htmlFor='password'>Mot de passe</label>
-                    <input className='rounded-2xl border-[1px] border-slate-300 p-2 ' placeholder='********' type='password' {...register('password')} />
+                    <label className='text-xs' htmlFor='password'>
+                        Mot de passe
+                    </label>
+                    <input
+                        className='rounded-2xl border-[1px] border-slate-300 p-2 '
+                        placeholder='********'
+                        type='password'
+                        {...register('password')}
+                    />
                     {errors.password && <p>{errors.password.message}</p>}
                 </div>
 
-                <button type='submit' className='m-auto my-2 w-2/3 rounded-2xl bg-primary px-8 py-2 font-semibold text-white hover:bg-primary/70'>Connectez-vous</button>
+                <button
+                    type='submit'
+                    className='m-auto my-2 w-2/3 rounded-2xl bg-primary px-8 py-2 font-semibold text-white hover:bg-primary/70'
+                >
+                    Connectez-vous
+                </button>
                 <div className='mt-2 justify-center text-center text-sm'>
                     <span>Vous n'êtes pas encore inscrit ? </span>
-                    <Link href='/sign-up' className='font-semibold underline'>Inscrivez-vous</Link>
+                    <Link href='/sign-up' className='font-semibold underline'>
+                        Inscrivez-vous
+                    </Link>
                 </div>
                 <div className='m-auto my-3 h-[1px] w-full bg-slate-300'></div>
                 <button
@@ -87,7 +112,12 @@ const SignInComponent = () => {
                     className='m-auto my-2 flex w-2/3 items-center justify-between rounded-2xl border-2 bg-slate-100 px-8 py-2 hover:bg-slate-300'
                 >
                     <span>Se connecter avec Google</span>
-                    <Image src='/images/google.svg' alt='google icon' width={20} height={20} />
+                    <Image
+                        src='/images/google.svg'
+                        alt='google icon'
+                        width={20}
+                        height={20}
+                    />
                 </button>
             </form>
         </section>
